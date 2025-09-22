9e665a2f837c6d75aeab7e85d2e999cf9c913595
AlicePisciuneri <alice.corleto@libero.it>
2025-09-22 18:33:56 +0200

    react-dc-comics

---
+++ react-dc-comics/src/components/AppFooter.jsx
@@ -0,0 +1,52 @@
+export default function AppFooter() {
+    return (
+        <footer>
+            <div className="footer-top">
+                <div>
+                    <h4>DC COMICS</h4>
+                    <ul>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>DC</h4>
+                    <ul>
+                        <li>Terms Of Use</li>
+                        <li>Privacy Policy</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>SITES</h4>
+                    <ul>
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="footer-bottom">
+                <button>SIGN-UP NOW!</button>
+                <div className="social">
+                    <span>FOLLOW US</span>
+                    <i className="fa-brands fa-facebook"></i>
+                    <i className="fa-brands fa-twitter"></i>
+                    <i className="fa-brands fa-youtube"></i>
+                    <i className="fa-brands fa-pinterest"></i>
+                    <i className="fa-brands fa-map-pin"></i>
+                </div>
+            </div>
+        </footer>
+    )
+}
---
+++ react-dc-comics/src/components/AppMain.jsx
@@ -0,0 +1,19 @@
+export default function AppMain() {
+    return (
+        <main>
+            <div className="jumbotron">
+                <p>--&gt; Content goes here  &lt;--</p>
+            </div>
+
+            <section className="blue-bar">
+                <ul>
+                    <li><a href="#">Digital Comics</a></li>
+                    <li><a href="#">DC Merchandise</a></li>
+                    <li><a href="#">Subscription</a></li>
+                    <li><a href="#">Comic Shop Locator</a></li>
+                    <li><a href="#">DC Power Visa</a></li>
+                </ul>
+            </section>
+        </main>
+    )
+}
